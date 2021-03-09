d2636eff6ec2adc1a93e15e4903ce181c7edf19f
DaniloToroL <danilo.toro@usach.cl>
2021-03-09 13:33:03 -0300

    fix encoding and new line to export

--- __init__.py
+++ __init__.py
@@ -36,13 +36,19 @@ if module == "read":
     path = GetParams("path")
     result = GetParams("result")
     delimiter = GetParams("delimiter")
+    encoding = GetParams("encoding")
 
     if not delimiter:
         delimiter = ","
+
+    if not encoding:
+        encoding = "utf-8"
     try:
+
         csv_result = []
-        with open(path) as csv_file:
+        with open(path, encoding=encoding) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=delimiter)
+            print(csv_reader)
             for row in csv_reader:
                 csv_result.append(row)
 
@@ -64,13 +70,16 @@ if module == "export":
         data = eval(data)
 
         if type(data[0]) is tuple:
+            print("Data is tuple")
             data = [list(da) for da in data]
         elif type(data[0]) is not list:
+            print("Data is not list")
             data = [data]
 
-        with open(path, mode='w') as csv_file:
+        with open(path, mode='w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=delimiter, quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for row in data:
+                print(row)
                 csv_writer.writerow(row)
     except Exception as e:
         print("\x1B[" + "31;40m" + str(e) + "\x1B[" + "0m")
